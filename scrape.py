163784c0900b2f7029132d6cab5ebc3da97333fc
Karen Lee <jonghl@microsoft.com>
2018-08-09 19:55:47 -0700

    Date formatting

--- scrape.py
+++ scrape.py
@@ -1,24 +1,22 @@
+import datetime
+
 import wget
 import datetime
 from wget import download
 import os
 
+fileExt = ".json.gz"
 
-filename = "<year>-0<month>-0<day>-<hour>.json.gz"
+fileTitle = datetime.datetime.now().strftime("%Y-%m-%d-{0..23}")
+fileName = fileTitle + fileExt
 
-today = datetime.datetime.today()
-filename = filename.replace("<year>" , str(today.year))
-filename = filename.replace("<month>", str(today.month))
-filename = filename.replace("<day>"  , str(today.day))
-filename = filename.replace("<hour>" , "0")#"{0..23}") #today.hour
-
-url = "http://data.gharchive.org/" + filename
+url = "http://data.gharchive.org/" + fileName
 outputDirectory = "files"
 
 print("Retrieving file: ")
-print(filename)
+print(fileName)
 print(" from url: ")
 print(url)
 
 #filename = wget.download(url, out=outputDirectory)
-urllib.urlretrieve(url, outputDirectory + "/" + filename)
+urllib.urlretrieve(url, outputDirectory + "/" + fileName)
