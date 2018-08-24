b7db3e14ef38b3d6927585dbd3e78ab9e4f954ee
Karen Lee <jonghl@microsoft.com>
2018-08-23 19:52:59 -0700

    refactor

--- scrape.py
+++ scrape.py
@@ -3,35 +3,36 @@ from urllib.request import Request, urlopen
 import gzip
 import json
 
+
+def get_file_title_with_date(target_date) :
+    return target_date.strftime("%Y-%m-%d-") + target_date.strftime("%H").replace('0', '', 1)
+
+
+def get_url(prefix, file_title, extension=None) :
+    return_url = prefix + file_title
+
+    if extension is not None:
+        return_url += ('.' + extension)
+
+    return return_url
+
+
+def get_response(url):
+    request = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    response = urlopen(request)
+
+    return response.read()
+
+
+def get_languages_url_from_json(json_data):
+    return json_data["payload"]["pull_request"]["head"]["repo"]["languages_url"]
+
+
 # We look at 1 hour delayed data.
-delayedTime = datetime.utcnow() - timedelta(hours=2)
-
-# fileTitle, for example, should be 2018-08-15-2 for 2018/8/15 2AM.
-# Month and day is zero padded but hour is not zero padded.
-fileTitle = delayedTime.strftime("%Y-%m-%d-") + delayedTime.strftime("%H").replace('0', '', 1)
-fileExt = ".json.gz"
-fileName = fileTitle + fileExt
-
-url = "http://data.gharchive.org/" + fileName
-outputDirectory = "files"
-
-print("Retrieving file: ")
-print(fileName)
-print(" from url: ")
-print(url)
-
-request = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-response = urlopen(request)
-
-compressedFile = response.read()
-decompressedFile = gzip.decompress(compressedFile)
-
-#TODO: Interpret this data as a string! That should fix the JSON interpreting problems, as the data is't a valid file
-print(type(decompressedFile))
-data = decompressedFile.decode('utf-8').replace('\n', '')
-print(type(data))
-
-jsonData = json.loads(data)
-
-print(jsonData["payload"]["pull_request"]["head"]["repo"]["languages_url"])
-#print(decompressedFile)
+delayed_time = datetime.utcnow() - timedelta(hours=2)
+title = get_file_title_with_date(delayed_time)
+url = get_url("http://data.gharchive.org/", title, "json.gz")
+response = get_response(url)
+data_file = gzip.decompress(response)
+data_list = data_file.decode('utf-8').split('\n')
+print(get_languages_url_from_json(json.loads(data_list[0])))
