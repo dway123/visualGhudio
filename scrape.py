6a0e2c0908dcf76c0684e7e8f286802dbd36cd73
Karen Lee <jonghl@microsoft.com>
2018-08-23 20:43:15 -0700

    catch errors

--- scrape.py
+++ scrape.py
@@ -25,14 +25,46 @@ def get_response(url):
 
 
 def get_languages_url_from_json(json_data):
-    return json_data["payload"]["pull_request"]["head"]["repo"]["languages_url"]
+    try:
+        return json_data["payload"]["pull_request"]["head"]["repo"]["languages_url"]
+    except KeyError:
+        #print("KeyError")
+        return None
+    except TypeError:
+        print("TypeError")
+        print("json_data is " + str(json_data) if json_data is not None else "None")
+        return None
+    except:
+        print("Some generic error")
+        print("json_data is " + str(json_data) if json_data is not None else "None")
+        return None
 
+
+def load_json(data):
+    try:
+        return json.loads(data)
+    except:
+        print("DataError")
+        print("data is " + str(data) if data is not None else "None")
+        print(type(data) if data is not None else "None")
+        return None
 
 # We look at 1 hour delayed data.
 delayed_time = datetime.utcnow() - timedelta(hours=2)
 title = get_file_title_with_date(delayed_time)
 url = get_url("http://data.gharchive.org/", title, "json.gz")
+print(url)
 response = get_response(url)
 data_file = gzip.decompress(response)
 data_list = data_file.decode('utf-8').split('\n')
-print(get_languages_url_from_json(json.loads(data_list[0])))
+
+language_urls = []
+for data in data_list:
+    language_url = get_languages_url_from_json(load_json(data))
+    if language_url is not None:
+        language_urls.append(language_url)
+
+print(language_urls)
+
+print(get_response(language_urls[0]))
+print(type(get_response(language_urls[0])))
